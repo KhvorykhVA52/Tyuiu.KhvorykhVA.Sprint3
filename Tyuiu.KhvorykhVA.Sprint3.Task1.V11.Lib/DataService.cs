4677fcc24500e092bede74cbecd222e1c51e18bd
Viktor Khvorih <Tyuiu.KhvorihVA@outlook.com>
2024-10-24 22:10:51 +0500

    Task1

--- Tyuiu.KhvorykhVA.Sprint3.Task1.V11.Lib/DataService.cs
+++ Tyuiu.KhvorykhVA.Sprint3.Task1.V11.Lib/DataService.cs
@@ -1,21 +1,22 @@
 ﻿using tyuiu.cources.programming.interfaces.Sprint3;
+
 namespace Tyuiu.KhvorykhVA.Sprint3.Task1.V11.Lib
 {
     public class DataService : ISprint3Task1V11
     {
         public double GetMultiplySeries(double value, int startValue, int stopValue)
         {
-            double multSeries = 1.0;
+            double multSeries = 1.0; 
             int k = startValue;
 
             while (k <= stopValue)
             {
-                double term = (Math.Pow(value, k)) + 0.5;  
+                double term = Math.Pow(value, k) + 0.5;  
                 multSeries *= term;
                 k++;
             }
 
-            return Math.Round(multSeries, 5); 
+            return Math.Round(multSeries, 5);  
         }
     }
 }
--- Tyuiu.KhvorykhVA.Sprint3.Task1.V11.Test/DataServiceTest.cs
+++ Tyuiu.KhvorykhVA.Sprint3.Task1.V11.Test/DataServiceTest.cs
@@ -9,16 +9,17 @@ namespace Tyuiu.KhvorykhVA.Sprint3.Task1.V11.Test
         [TestMethod]
         public void GetMultiplySeries()
         {
+            
             DataService ds = new DataService();
             double value = 0.75;
             int startValue = 1;
             int stopValue = 14;
 
-            double res = ds.GetMultiplySeries(value, startValue, stopValue);
-            double expected = 0.005;
-
             
-            Assert.AreEqual(expected, res);
+            double res = ds.GetMultiplySeries(value, startValue, stopValue);
+            double expected = 0.005;  
+            double delta = 0.0001; 
+            Assert.AreEqual(expected, res, delta, "The result of GetMultiplySeries is not as expected.");
         }
     }
 }
