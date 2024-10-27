76dcd9ec12cadd89fc8067e8f22e5675632bde11
Viktor Khvorih <Tyuiu.KhvorihVA@outlook.com>
2024-10-27 16:24:41 +0500

    Task4

--- Tyuiu.KhvorykhVA.Sprint3.Task4.V20.Test/DataServiceTest.cs
+++ Tyuiu.KhvorykhVA.Sprint3.Task4.V20.Test/DataServiceTest.cs
@@ -1,4 +1,6 @@
 using Tyuiu.KhvorykhVA.Sprint3.Task4.V20.Lib;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
 namespace Tyuiu.KhvorykhVA.Sprint3.Task4.V20.Test
 {
     [TestClass]
@@ -11,8 +13,8 @@ namespace Tyuiu.KhvorykhVA.Sprint3.Task4.V20.Test
             int startValue = -5;
             int stopValue = 5;
             double res = ds.Calculate(startValue, stopValue);
-            double wait = 10.48303856012738;
-            Assert.AreEqual(wait,res);
+            double wait = 10.483;
+            Assert.AreEqual(wait, res, 0.001);
         }
     }
 }
